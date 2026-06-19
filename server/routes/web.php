336170db87533550a9be8863a5f418fe236600b8
kiruhach <pochta23891@inbox.ru>
2026-06-20 02:39:46 +1000

    Fix SPA routing: remove wildcard route, use 404 handler. Fix SESSION_DOMAIN

--- server/routes/web.php
+++ server/routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->file(public_path('index.html'));
 });
-
-Route::get('/{any}', function () {
-    return response()->file(public_path('index.html'));
-})->where('any', '^(?!admin).*$');
