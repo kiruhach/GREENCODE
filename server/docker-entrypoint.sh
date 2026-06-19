71df69814ac7e56f482bda9d553a51fbb023c5a1
kiruhach <pochta23891@inbox.ru>
2026-06-20 04:27:48 +1000

    Debug: enable APP_DEBUG=true, add info.php

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 sed -i "s/__PORT__/${PORT:-8080}/g" /etc/nginx/nginx.conf
 
+echo '<?php phpinfo(); ?>' > /var/www/html/public/info.php
+
 php artisan migrate --force || echo "Migration failed, will retry on next restart"
 php artisan db:seed --force || echo "Seeding failed, will retry on next restart"
 
