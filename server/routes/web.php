78bcc846c6b53da7dcb70c036bf33da83c9e30ab
kiruhach <pochta23891@inbox.ru>
2026-06-20 03:10:59 +1000

    Diagnostic: exclude Livewire from CSRF, add /debug route

--- server/routes/web.php
+++ server/routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->file(public_path('index.html'));
 });
+
+Route::get('/debug', function () {
+    $sessionId = session()->getId();
+    $token = csrf_token();
+    $cookies = request()->cookies->all();
+    $sessionData = session()->all();
+
+    return response()->json([
+        'session_id' => $sessionId,
+        'csrf_token' => $token,
+        'session_has' => session()->has('_token'),
+        'session_data_keys' => array_keys($sessionData),
+        'cookies' => $cookies,
+    ]);
+});
